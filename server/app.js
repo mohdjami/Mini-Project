1bd6f071f4522c01151509ebf40ab534fa370728
mohdjami <mohdjamikhann@gmail.com>
2024-01-30 17:31:36 +0530

    Testing Prisma

--- server/app.js
+++ server/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 const app = express();
+//Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
 const port = 3000;
-app.get("/", (req, res) => {
+
+// app.use("/api/auth", authRouter);
+app.get("/", async (req, res) => {
+  await prisma.lawyer.create({
+    data: {
+      name: "Anant",
+      email: "Anant@mail.com",
+      password: "1234",
+    },
+  });
   res.send("Hello World!");
 });
 
--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = require("./app");
 const port = 3000;
 
-//Body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
