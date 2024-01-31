4b85cb8f4027a5b478e56a14f39846ee861ef3fb
mohdjami <mohdjamikhann@gmail.com>
2024-01-31 15:36:06 +0530

    Added authentication and Authorization

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,49 +1,88 @@
 const { PrismaClient } = require("@prisma/client");
 const { router } = require("../app");
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 const prisma = new PrismaClient();
+
+const createToken = async (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const lawyer = await prisma.lawyer.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
         email,
       },
     });
-    const passwordValid = argon2.verify(lawyer.password, password);
+    if (!user) {
+      res.status(404).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+    const token = await createToken(user.id);
+    const passwordValid = argon2.verify(user.password, password);
     if (!passwordValid) {
-      throw new Error("Invalid password");
+      res.status(400).json({
+        status: "fail",
+        message: "Incorrect password",
+      });
     }
     res.status(200).json({
       status: "success",
       data: {
-        lawyer,
+        user,
+        token,
       },
     });
   } catch (error) {
-    res.send(error);
+    console.log(error);
+    res.status(200).json({
+      status: "fail",
+      message: error,
+    });
   }
 };
 
 exports.signup = async (req, res, next) => {
   try {
     const { email, password, name, phone, address, description } = req.body;
-
+    if (!email || !password || !name) {
+      res.status(400).json({
+        status: "fail",
+        message: "Email, password and name are required",
+      });
+    }
     const hash = await argon2.hash(password);
-    const lawyer = await prisma.lawyer.create({
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (existingUser) {
+      res.status(400).json({
+        status: "fail",
+        message: "Email already exists",
+      });
+    }
+    const user = await prisma.user.create({
       data: {
         email,
         password: hash,
         name,
       },
     });
-    console.log(lawyer);
+    const token = await createToken(user.id);
     res.status(200).json({
       status: "success",
       data: {
-        lawyer,
+        user,
       },
+      token,
     });
   } catch (error) {
     console.log(error);
